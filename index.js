48eaf5f9d2c0916566fd9adca9e4fb102528bf60
kevduc <kduconge@gmail.com>
2021-09-29 05:11:10 +0100

    Splitting description lines

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const tables = [...document.querySelector('#contenu_corps_central').querySelecto
 
 const legumes = tables.map((table) => [
   table.previousElementSibling.innerText,
-  [...table.rows].map((row) => [...row.cells[1].children].map((element) => element.innerText)),
+  [...table.rows].map((row) => {
+    const [name, description] = [...row.cells[1].children].map((element) => element.innerText)
+    return [name, description.replace(/^\n/, '').split('\n')]
+  }),
 ])
 
 console.log(JSON.stringify(legumes))
