bd5233abacaafba87d35dacd12c7d24653991c17
kevduc <kduconge@gmail.com>
2021-09-29 08:21:05 +0100

    Added veggie properties: season, calcium and oxalates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import { JSDOM } from 'jsdom'
 
-const outputFile = 'vegetables.json'
+const outputFile = 'vegetable-lists.json'
 const inputFile = 'input.html'
 const url = 'https://www.ladureviedulapinurbain.com/listelegumes.php'
 
@@ -20,23 +20,42 @@ const { document } = new JSDOM(rawText).window
 
 const tables = [...document.querySelector('#contenu_corps_central').querySelectorAll('table')]
 
-const veggies = tables.map((table) => [
-  table.previousElementSibling.textContent,
-  [...table.rows].map((row) => {
-    const [name, ...description] = [...row.cells[1].childNodes]
-      .map((node) => node.textContent ?? node.data)
-      .filter((text) => !/^\s*$/.test(text))
-    return [
-      name,
-      description.flatMap((text) =>
-        text
-          .trim()
-          .split(/[\n\.]/)
-          .map((text) => text.trim())
-          .filter((text) => text.length > 0)
-      ),
-    ]
-  }),
-])
+const veggieProperties = ['saison', 'taux de calcium', 'oxalates']
+const veggiePropertiesRegex = new RegExp(`^\\s*(${veggieProperties.join('|')})\\s*:?(.*)`, 'i')
+
+const veggies = {
+  listes: tables.map((table) => ({
+    description: table.previousElementSibling.textContent.trim().replace(/\s*:\s*/, ''),
+    legumes: [...table.rows].map((row) => {
+      const [name, ...info] = [...row.cells[1].childNodes]
+        .map((node) => node.textContent ?? node.data)
+        .filter((text) => !/^\s*$/.test(text))
+
+      const vegetable = { nom: name }
+      info
+        .flatMap((text) =>
+          text
+            .trim()
+            .split(/[\n\.]/)
+            .map((text) => text.trim())
+            .filter((text) => text.length > 0)
+        )
+        .forEach((text) => {
+          const [, property, value] = text.match(veggiePropertiesRegex) ?? [, 'description', text]
+          const cleanProperty = property.trim().toLowerCase().replace(/\s/g, '_')
+          const cleanValue = value.trim()
+
+          vegetable[cleanProperty] =
+            vegetable[cleanProperty] !== undefined ? `${vegetable[cleanProperty]}\n${cleanValue}` : `${cleanValue}`
+        })
+
+      return {
+        ...vegetable,
+        description: vegetable.description?.split('\n'),
+        saison: vegetable.saison?.split(',').map((text) => text.trim().toLowerCase()),
+      }
+    }),
+  })),
+}
 
 fs.writeFileSync(outputFile, JSON.stringify(veggies, null, 3))
